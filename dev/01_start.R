ef21b3d154d2f3fb970238cd1238641abd759e7a
Daniel Flores Agreda <daniel.flores@epfl.ch>
2021-08-16 08:36:12 +0200

    Initial commit

--- dev/01_start.R
+++ dev/01_start.R
@@ -20,11 +20,11 @@
 ## 
 golem::fill_desc(
   pkg_name = "stockExamplewithGolem", # The Name of the package containing the App 
-  pkg_title = "PKG_TITLE", # The Title of the package containing the App 
-  pkg_description = "PKG_DESC.", # The Description of the package containing the App 
-  author_first_name = "AUTHOR_FIRST", # Your First Name
-  author_last_name = "AUTHOR_LAST", # Your Last Name
-  author_email = "AUTHOR@MAIL.COM", # Your Email
+  pkg_title = "Stocks Example", # The Title of the package containing the App 
+  pkg_description = "This is an example of implementation of a golem package for stocks analsysi", # The Description of the package containing the App 
+  author_first_name = "Daniel", # Your First Name
+  author_last_name = "Flores Agreda", # Your Last Name
+  author_email = "daniel.flores@epfl.ch", # Your Email
   repo_url = NULL # The URL of the GitHub Repo (optional) 
 )     
 
@@ -34,7 +34,7 @@ golem::set_golem_options()
 ## Create Common Files ----
 ## See ?usethis for more information
 usethis::use_mit_license( "Golem User" )  # You can set another license here
-usethis::use_readme_rmd( open = FALSE )
+usethis::use_readme_rmd(open = FALSE)
 usethis::use_code_of_conduct()
 usethis::use_lifecycle_badge( "Experimental" )
 usethis::use_news_md( open = FALSE )
